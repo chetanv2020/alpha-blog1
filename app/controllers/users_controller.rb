5f3b43e577dd3479321408c55ef63b0d44ff0e0e
Rails course <chetanv2020@gmail.com>
2017-04-16 16:57:07 +0000

    log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   
+  before_action :set_user , only: [:edit, :update, :show]
+  
   def index
     
     @users = User.paginate(page: params[:page], per_page:3)
@@ -22,14 +24,10 @@ class UsersController < ApplicationController
   end 
   
   def edit
-    
-   @user = User.find(params[:id])
-  
+
   end
   
   def update
-    @user = User.find(params[:id])
-    
     if @user.update(user_params)
       flash[:sucess] = "Your account was updated sucessfully"
       redirect_to articles_path
@@ -41,10 +39,12 @@ class UsersController < ApplicationController
   end
   
   def show
-    
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page:5)
   
+  end
+  
+  def set_user
+       @user = User.find(params[:id])
   end
   
   
